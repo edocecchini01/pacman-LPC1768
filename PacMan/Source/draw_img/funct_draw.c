f619ea763513ce1d3bf7b95146fb14c8b7288813
Antonio Ceglia <a.ceglia03@gmail.com>
2024-12-13 10:19:17 +0100

    draw 1, 2

--- PacMan/Source/draw_img/funct_draw.c
+++ PacMan/Source/draw_img/funct_draw.c
@@ -4,8 +4,18 @@
 //LCD_DrawLine( uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1 , uint16_t color );
 
 
-void draw_tail1(uint16_t x0, uint16_t y0)
+void draw_tail_1(uint16_t x0, uint16_t y0)
 {
 	LCD_DrawLine(x0+6, y0+3, x0+5, y0+3 ,Blue);
+	LCD_SetPoint(x0+4, y0+4, Blue);
+	LCD_DrawLine(x0+3, y0+5, x0+3, y0+6 ,Blue);
+
+	LCD_SetPoint(x0, y0, Green);
+}
+
+void draw_tail_2(uint16_t x0, uint16_t y0)
+{
+	LCD_DrawLine(x0, y0+3, x0+6, y0+3 ,Blue);
+
 	LCD_SetPoint(x0, y0, Green);
 }
